b9eed832a97c43d79cbeb07f2a2344dfcc6844f2
jarppe <jarppe@metosin.fi>
2015-01-14 18:39:07 +0200

    init

---
+++ test/clojure_bootcamp_20150130/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-bootcamp-20150130.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-bootcamp-20150130.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
